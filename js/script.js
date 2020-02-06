d00243a1a68e68c5834992aaa0341843ba6cfea8
dahwindelacruz <dahwindelacruz@gmail.com>
2020-02-06 14:57:52 -0800

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //STEP 1
-//Convert the following identifiers to Camel Case notation:
+// Convert the following identifiers to Camel Case notation:
 
 // var someMonth;
 // function theMonth();
@@ -9,7 +9,7 @@
 
 
 //STEP 2
-//Give me an example of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression.
+// Give me an example of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression.
 
 // 1.9
 // "Web developer"
@@ -17,14 +17,14 @@
 
 
 //STEP 3
-//Give me two examples of complex / variable expressions.
+// Give me two examples of complex / variable expressions.
 
 // let milesPerGallon = totalMilesDriven / gallonsInTank;
 // let volume = width * length * height;
 
 
 //STEP 4
-//Declare (but do not assign) 9 variables for the following identifiers: First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. Use Camel Casing and Hungarian Notation when naming your identifiers.
+// Declare (but do not assign) 9 variables for the following identifiers: First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. Use Camel Casing and Hungarian Notation when naming your identifiers.
 
 // let firstName;
 // let lastName;
@@ -38,4 +38,57 @@
 
 
 //STEP 5
-//Take the 9 variables that you created above and demonstrate 3 methods for declaring and assigning values to those variables.
+// Take the 9 variables that you created above and demonstrate 3 methods for declaring and assigning values to those variables.
+
+// 1st method
+// let firstName;
+// let lastName;
+// let address;
+// firstName = Dahwin;
+// lastName = DelaCruz;
+// address = "In San Diego";
+
+// 2nd method
+// let city = "San Diego"
+// let state = CA
+// let zipCode = "92120"
+
+// 3rd method
+// let yourAge = 38, referralSource = web, mayWeContactYou = yes;
+
+
+//STEP 6
+// Create a variable. 
+// Add a number and a string and display the coerced result in the browser’s console window. 
+
+// let myAge = 38;
+// window.console.log("Dahwin is " + myAge + " years old.");
+
+
+//STEP 7
+// Create two variables. 
+// For the first variable, add a Boolean and a string and display the coerced result. 
+// For the second variable, add a number and a Boolean and display the coerced result. 
+
+// let realName = true + " is his real name";
+// window.console.log(realName);
+
+// let realAge = 38 + true;
+// window.console.log(realAge);
+
+
+//STEP 8
+// Is the following string literal valid? If not, how would you fix it?
+
+// var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."';
+// window.console.log(someString);
+
+// invalid, fix shown below
+// let someString = "Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\"";
+// window.console.log(someString);
+
+
+//STEP 9
+// Create a variable that produces a null value in the console window. 
+// Create a variable that produces an undefined value in the console window.
+
