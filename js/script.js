558f8dc38bbe656d337e63f5b8af6efadb37cb56
dahwindelacruz <dahwindelacruz@gmail.com>
2020-02-06 12:20:03 -0800

    update

---
+++ js/script.js
@@ -0,0 +1,41 @@
+//STEP 1
+//Convert the following identifiers to Camel Case notation:
+
+// var someMonth;
+// function theMonth();
+// var currentMonth;
+// var summerMonth;
+// var myLibraryFunction;
+
+
+//STEP 2
+//Give me an example of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression.
+
+// 1.9
+// "Web developer"
+// true
+
+
+//STEP 3
+//Give me two examples of complex / variable expressions.
+
+// let milesPerGallon = totalMilesDriven / gallonsInTank;
+// let volume = width * length * height;
+
+
+//STEP 4
+//Declare (but do not assign) 9 variables for the following identifiers: First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. Use Camel Casing and Hungarian Notation when naming your identifiers.
+
+// let firstName;
+// let lastName;
+// let address;
+// let city;
+// let state;
+// let zipCode;
+// let yourAge;
+// let referralSource;
+// let mayWeContactYou;
+
+
+//STEP 5
+//Take the 9 variables that you created above and demonstrate 3 methods for declaring and assigning values to those variables.
